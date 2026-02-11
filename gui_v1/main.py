afce2b77e4560a538b0e46e06956ee3bb0ff3ab2
BDorian332 <barreredorian332@gmail.com>
2026-02-11 23:50:47 +0100

    added text editing

--- gui_v1/main.py
+++ gui_v1/main.py
@@ -25,21 +25,25 @@ class ITPTGUI:
         # ---- Preview state ----
 
         self.preview_image = None
+        self.zoomed_image = None
         self.tk_image = None
         self.zoom = 1.0
+        self.last_zoom = self.zoom
         self.pan_x = 0
         self.pan_y = 0
+        self.last_pan = (self.pan_x, self.pan_y)
         self.drag_start = None
+        self.drag_type = None # "left" or "right"
 
-        # ---- Points ----
+        # ---- Points and Texts ----
 
         self.points = []  # list of Point objects
-        self.add_mode = None  # "node", "corner", None
         self.selected_point = None  # for drag
 
-        # ---- Texts ----
-
         self.texts = []
+        self.selected_text_id = None
+
+        self.add_mode = None  # "node", "corner", "text" ou None
 
         # ---- UI ----
 
@@ -80,12 +84,18 @@ class ITPTGUI:
         self.preview_canvas.grid(row=4, column=0, columnspan=3, sticky="nsew", padx=5, pady=5)
 
         # Point buttons (toggle exclusive)
-        self.add_node_btn = ttk.Button(root, text="Add Node", command=lambda: self.toggle_mode("node"), state="disabled")
-        self.add_corner_btn = ttk.Button(root, text="Add Corner", command=lambda: self.toggle_mode("corner"), state="disabled")
-        self.add_node_btn.grid(row=5, column=0, padx=5, pady=5, sticky="w")
-        self.add_corner_btn.grid(row=5, column=1, padx=5, pady=5, sticky="w")
-        self.clear_points_btn = ttk.Button(root, text="Clear Points", command=self.clear_points, state="disabled")
-        self.clear_points_btn.grid(row=5, column=2, padx=5, pady=5, sticky="w")
+        edit_buttons_frame = ttk.Frame(root)
+        edit_buttons_frame.grid(row=5, column=0, columnspan=3, sticky="w")
+        self.add_node_btn = ttk.Button(edit_buttons_frame, text="Add Node", command=lambda: self.toggle_mode("node"), state="disabled")
+        self.add_corner_btn = ttk.Button(edit_buttons_frame, text="Add Corner", command=lambda: self.toggle_mode("corner"), state="disabled")
+        self.clear_points_btn = ttk.Button(edit_buttons_frame, text="Clear All Points (right click on one to delete it)", command=self.clear_points, state="disabled")
+        self.add_text_btn = ttk.Button(edit_buttons_frame, text="Add Text", command=lambda: self.toggle_mode("text"), state="disabled")
+        self.clear_texts_btn = ttk.Button(edit_buttons_frame, text="Clear All Texts (right click on one to delete it)", command=self.clear_texts, state="disabled")
+        self.add_node_btn.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        self.add_corner_btn.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+        self.clear_points_btn.grid(row=0, column=2, padx=5, pady=5, sticky="w")
+        self.add_text_btn.grid(row=0, column=3, padx=5, pady=5, sticky="w")
+        self.clear_texts_btn.grid(row=0, column=4, padx=5, pady=5, sticky="w")
 
         # Output text
         ttk.Label(root, text="Output:").grid(row=6, column=0, columnspan=3, sticky="w", padx=5, pady=5)
@@ -160,8 +170,12 @@ class ITPTGUI:
         # Hover cursor
         c.bind("<Motion>", self.update_cursor)
 
+        # Model change
         self.model_combobox.bind("<<ComboboxSelected>>", self.on_model_change)
         self.on_model_change()
+
+        # Bind edit text
+        self.preview_canvas.tag_bind("text", "<Double-1>", self.edit_text)
 
     def on_model_change(self, event=None):
         self.update_steps_ui()
@@ -175,15 +189,9 @@ class ITPTGUI:
             self.add_mode = mode
 
         # Ensure mutual exclusivity
-        if self.add_mode == "node":
-            self.add_node_btn.state(["pressed"])
-            self.add_corner_btn.state(["!pressed"])
-        elif self.add_mode == "corner":
-            self.add_node_btn.state(["!pressed"])
-            self.add_corner_btn.state(["pressed"])
-        else:
-            self.add_node_btn.state(["!pressed"])
-            self.add_corner_btn.state(["!pressed"])
+        self.add_node_btn.state(["pressed"] if self.add_mode=="node" else ["!pressed"])
+        self.add_corner_btn.state(["pressed"] if self.add_mode=="corner" else ["!pressed"])
+        self.add_text_btn.state(["pressed"] if self.add_mode=="text" else ["!pressed"])
 
     # ---------- File ----------
 
@@ -212,11 +220,15 @@ class ITPTGUI:
             self.add_node_btn.config(state="normal")
             self.add_corner_btn.config(state="normal")
             self.clear_points_btn.config(state="normal")
+            self.add_text_btn.config(state="normal")
+            self.clear_texts_btn.config(state="normal")
         except Exception:
             self.preview_image = None
             self.add_node_btn.config(state="disabled")
             self.add_corner_btn.config(state="disabled")
             self.clear_points_btn.config(state="disabled")
+            self.add_text_btn.config(state="disabled")
+            self.clear_texts_btn.config(state="disabled")
         self.points.clear()
         self.redraw_preview()
 
@@ -224,6 +236,7 @@ class ITPTGUI:
         self.preview_canvas.delete("all")
         if not self.preview_image:
             return
+
         canvas_w = self.preview_canvas.winfo_width()
         canvas_h = self.preview_canvas.winfo_height()
         img_w = self.preview_image.width
@@ -233,10 +246,17 @@ class ITPTGUI:
         self.base_ratio = base_ratio
 
         # Draw image
-        new_w = int(img_w * ratio)
-        new_h = int(img_h * ratio)
-        resized = self.preview_image.resize((new_w, new_h))
-        self.tk_image = ImageTk.PhotoImage(resized)
+        if self.zoomed_image is None or self.zoom != self.last_zoom:
+            new_w = int(self.preview_image.width * ratio)
+            new_h = int(self.preview_image.height * ratio)
+            self.zoomed_image = self.preview_image.resize((new_w, new_h), Image.NEAREST)
+            self.last_zoom = self.zoom
+            self.last_pan = None
+
+        if self.last_pan is None or self.last_pan != (self.pan_x, self.pan_y):
+            self.tk_image = ImageTk.PhotoImage(self.zoomed_image)
+            self.last_pan = (self.pan_x, self.pan_y)
+
         self.preview_canvas.create_image(
             canvas_w//2 + self.pan_x,
             canvas_h//2 + self.pan_y,
@@ -250,6 +270,15 @@ class ITPTGUI:
             color = "red" if pt.type == "node" else "blue"
             r = 5
             self.preview_canvas.create_oval(screen_x-r, screen_y-r, screen_x+r, screen_y+r, fill=color, outline="black")
+
+        # Draw texts
+        for i, txt in enumerate(self.texts):
+            x1, y1, x2, y2 = txt["bbox"]
+            cx_img = x1
+            cy_img = y1 + (y2 - y1) / 2
+            cx, cy = self.image_to_screen(Point(cx_img, cy_img))
+
+            self.preview_canvas.create_text(cx, cy, text=txt["text"], tags=("text", f"text_{i}"), fill="red", font=("Arial", 12), anchor="w")
 
     # ---------- Coordinate transforms ----------
 
@@ -291,24 +320,53 @@ class ITPTGUI:
                 return pt
         return None
 
+    def get_hovered_text_id(self, event):
+        items = self.preview_canvas.find_overlapping(
+            event.x, event.y,
+            event.x, event.y
+        )
+
+        for item in reversed(items):
+            for tag in self.preview_canvas.gettags(item):
+                if tag.startswith("text_"):
+                    return int(tag.split("_")[1])
+        return None
+
     def start_interaction(self, event):
-        # Check if clicking on a point
+        # Check if clicking on a point or a text
         self.selected_point = self.get_hovered_point(event)
+        self.selected_text_id = self.get_hovered_text_id(event)
 
         if event.num == 3:
             self.drag_start = (event.x, event.y)
-        else:
-            if self.selected_point is None and self.add_mode:
-                # Add point
-                x, y = self.screen_to_image((event.x, event.y))
-                self.points.append(Point(x, y, self.add_mode))
-                self.redraw_preview()
-            elif self.selected_point is None:
-                self.drag_start = (event.x, event.y)
+            self.drag_type = "right"
+            return
+
+        if self.selected_point is not None:
+            return
+
+        if self.selected_text_id is not None or self.add_mode is None:
+            self.drag_start = (event.x, event.y)
+            self.drag_type = "left"
+            return
+
+        if self.add_mode == "node" or self.add_mode == "corner":
+            # Add point
+            x, y = self.screen_to_image((event.x, event.y))
+            self.points.append(Point(x, y, self.add_mode))
+            self.redraw_preview()
+        elif self.add_mode == "text":
+            x, y = self.screen_to_image((event.x, event.y))
+            self.texts.append({
+                "text": "Text",
+                "bbox": (x, y, x, y)
+            })
+            self.redraw_preview()
 
     def do_interaction(self, event):
-        if self.drag_start is not None:
+        if self.drag_type == "right" or (self.drag_type == "left" and self.selected_point is None and self.selected_text_id is None):
             self.selected_point = None
+            self.selected_text_id = None
 
             # Drag image
             dx = event.x - self.drag_start[0]
@@ -316,39 +374,91 @@ class ITPTGUI:
             self.pan_x += dx
             self.pan_y += dy
             self.drag_start = (event.x, event.y)
-            if self.zoom < 4:
-                self.redraw_preview()
-        elif self.selected_point:
+        elif self.selected_point is not None:
             # Drag point
             x, y = self.screen_to_image((event.x, event.y))
             self.selected_point.x = x
             self.selected_point.y = y
-            self.redraw_preview()
+        elif self.selected_text_id is not None:
+            txt = self.texts[self.selected_text_id]
+            px, py = self.screen_to_image(self.drag_start)
+            cx, cy = self.screen_to_image((event.x, event.y))
+            dx = cx - px
+            dy = cy - py
+            x1, y1, x2, y2 = txt["bbox"]
+            txt["bbox"] = (
+                x1 + dx,
+                y1 + dy,
+                x2 + dx,
+                y2 + dy,
+            )
+            self.drag_start = (event.x, event.y)
+
+        self.redraw_preview()
 
     def end_interaction(self, event):
         if self.zoom >= 4:
                 self.redraw_preview()
 
-        if event.num == 3 and self.selected_point is not None:
-            self.points.remove(self.selected_point)
+        if event.num == 3:
+            if self.selected_point is not None:
+                self.points.remove(self.selected_point)
+            elif self.selected_text_id is not None:
+                self.texts.pop(self.selected_text_id)
             self.redraw_preview()
 
         self.drag_start = None
+        self.drag_type = None
         self.selected_point = None
+        self.selected_text_id = None
 
     def clear_points(self):
         self.points.clear()
         self.redraw_preview()
 
+    def clear_texts(self):
+        self.texts.clear()
+        self.redraw_preview()
+
     def update_cursor(self, event):
-        cursor_hand = False
-
         hovered_point = self.get_hovered_point(event)
+        hovered_text = self.get_hovered_text_id(event)
 
-        if hovered_point is not None:
-            cursor_hand = True
+        if hovered_point is not None or hovered_text is not None:
+            self.preview_canvas.config(cursor="hand2")
+        else:
+            self.preview_canvas.config(cursor="")
 
-        self.preview_canvas.config(cursor="hand2" if cursor_hand else "")
+    def edit_text(self, event):
+        index = self.get_hovered_text_id(event)
+
+        x1, y1, x2, y2 = self.texts[index]["bbox"]
+        cx_img = x1 + (x2 - x1) / 2
+        cy_img = y1 + (y2 - y1) / 2
+        cx, cy = self.image_to_screen(Point(cx_img, cy_img))
+
+        old_text = self.texts[index]["text"]
+
+        entry = tk.Entry(self.preview_canvas)
+        entry.insert(0, self.texts[index]["text"])
+
+        entry_width = 110
+        entry_height = 30
+        entry.place(x=cx - entry_width/2, y=cy - entry_height/2, width=entry_width, height=entry_height)
+        entry.focus_set()
+
+        def save_text(event):
+            self.texts[index]["text"] = entry.get()
+            entry.destroy()
+            self.redraw_preview()
+
+        def cancel_edit(event=None):
+            entry.destroy()
+            self.redraw_preview()
+
+        entry.bind("<Return>", save_text)
+        entry.bind("<FocusOut>", save_text)
+        entry.bind("<Escape>", cancel_edit)
 
     # ---------- Output ----------
 
@@ -372,6 +482,11 @@ class ITPTGUI:
             img_w = self.preview_image.width
             img_h = self.preview_image.height
 
+            self.texts = [{
+                "text": "test",
+                "bbox": (20, 20, 70, 50)
+            }]
+
             model = get_model(model_name)
             model.load()
 
@@ -391,7 +506,6 @@ class ITPTGUI:
 
                     self.points = scale_points(points, scale_width=img_w, scale_height=img_h)
                     self.texts = scale_texts(texts, scale_width=img_w, scale_height=img_h)
-                    self.redraw_preview()
 
                     newick_str = newick.to_string()
 
@@ -408,6 +522,7 @@ class ITPTGUI:
             self.root.after(0, self.progress.stop)
             self.root.after(0, self.progress.grid_remove)
             self.root.after(0, lambda: self.convert_button.config(state="normal"))
+            self.root.after(0, self.redraw_preview)
 
     def convert(self):
         self.convert_button.config(state="disabled")
