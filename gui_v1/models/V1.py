6a8f5a76aa6f42cda6a5bb1d9c60165cc557864a
Mattéo REYNE <132447227+Asaschi@users.noreply.github.com>
2026-02-17 14:10:29 +0100

    Modification du à l'ajout de nodesdetection dans V1.py

--- gui_v1/models/V1.py
+++ gui_v1/models/V1.py
@@ -1,5 +1,6 @@
 from typing import List
 from gui_v1.main import Step
+from itpt.core.newick import Point, build_newick
 
 STEPS = [
     Step("Extract Tree", default_enabled=True),
@@ -18,8 +19,9 @@ def run_steps(model, img_rgb, steps=None):
     if clean_step and clean_step.enabled:
         output = model.clean_tree(output)
 
-    nodes_by_image = model.detect_nodes(output)
+    nodes_by_image, corners_by_image = model.detect_nodes(output)
     nodes = nodes_by_image[0]
+    corners = corners_by_image[0]
 
     detect_texts_step = next((s for s in steps if s.name == "Detect Texts"), None)
     if detect_texts_step and detect_texts_step.enabled:
@@ -28,6 +30,9 @@ def run_steps(model, img_rgb, steps=None):
     else:
         texts = []
 
-    newick = model.build_newick(nodes, texts=texts)
+    points = [Point(x, y, "node") for (x, y, _) in nodes] + \
+             [Point(x, y, "corner") for (x, y, _) in corners]
+
+    newick = build_newick(points, texts=texts)
 
     return newick, points, texts
